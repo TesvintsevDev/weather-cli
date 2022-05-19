408d363b5cebd1af7d54e9e0d01d61ab8c411851
TesvintsevDev <tesvintsev@gmail.com>
2022-05-19 11:46:26 +0300

    output to the console

---
+++ services/log.service.js
@@ -0,0 +1,26 @@
+// Обрабатываем вывод в консоль
+// Библиотека chalk добавляет различные цвета в консоль
+// Библиотека dedent используется для стайла отображения строки в коде при конкатенации в ``
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const printError = (error) => {
+	console.log(chalk.bgRed(' ERROR ') + ' ' + error);
+};
+
+const printSuccess = (message) => {
+	console.log(chalk.bgGreen(' SUCCESS ') + ' ' + message);
+};
+
+const printHelp = () => {
+	console.log(
+		dedent`${chalk.bgCyan(' HELP ')}
+		Без параметров - вывод погоды
+		-s [CITY] для установки города
+		-h для вывода помощи
+		-t [API_KEY] для сохранения токена
+		`
+	);
+};
+
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log.service.js'
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
-	console.log(args)
+	if (args.h) {
+		printHelp()
+	}
 }
 
 initCLI()
