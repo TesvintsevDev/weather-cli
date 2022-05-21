f13cbaf7f8b05550725314205bbef2c32af057a3
TesvintsevDev <tesvintsev@gmail.com>
2022-05-21 11:44:55 +0300

    working with the file system

---
+++ services/storage.service.js
@@ -0,0 +1,38 @@
+// Работа с файловой системой
+// Получение и сохранение токена
+
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const saveKeyValue = async (key, value) => {
+  let data = {};
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
+};
+
+const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+};
+
+const isExist = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+export { saveKeyValue, getKeyValue };
--- weather.js
+++ weather.js
@@ -1,12 +1,28 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js'
+import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
 
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Токен сохранён');
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+// Вызывает наборы изолированных функций
 const initCLI = () => {
-	const args = getArgs(process.argv)
-	if (args.h) {
-		printHelp()
-	}
-}
+  const args = getArgs(process.argv);
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+  }
+  if (args.t) {
+    return saveToken(args.t);
+  }
+};
 
-initCLI()
+initCLI();
